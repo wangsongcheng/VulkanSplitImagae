37be9024f5064b4c584c625abef58e9c777afea1
wangsongcheng <949268935@qq.com>
2023-12-16 13:46:20 +0800

    backup;完整图片输出有问题;拼图功能未写

--- shaders/compile.sh
+++ shaders/compile.sh
@@ -1,5 +1,7 @@
 glslangValidator -V base.vert -o baseVert.spv
 glslangValidator -V base.frag -o baseFrag.spv
 
+glslangValidator -V debug.frag -o debugFrag.spv
+
 glslangValidator -V baseColor.vert -o baseColorVert.spv
 glslangValidator -V baseColor.frag -o baseColorFrag.spv
